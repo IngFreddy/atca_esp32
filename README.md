7988f8986afe5ec9d300538da81e7561bb094d70
Milan <xmucka02@stud.fit.vutbr.cz>
2018-08-04 19:21:15 +0200

    Example modified

--- README.md
+++
@@ -1,2 +0,0 @@
-# atca_esp32
-CryptoAuthLib - Microchip CryptoAuthentication Library on ESP32 with AES encrypt
--- main/crypto_esp.h
+++ main/crypto_esp.h
@@ -22,8 +22,6 @@
 #include "atca_host.h"
 #include "atca_basic.h"
 
-#include "provision.h"
-
 
 #define MSG_NONCE_SIZE	32
 #define ATCA_HAL_I2C
@@ -33,7 +31,19 @@
 #define ASYMETRIC_KEY_SLOT	4
 #define TRANSPORT_KEY_SLOT	2
 
-const ATCAIfaceCfg cfg_ateccx08a_i2c = {
+
+#define CHECK_STATUS(s)  if(s != ATCA_SUCCESS) { printf("Error: Line %d in %s\r\n", __LINE__, __FILE__); while(1);	}
+
+#define PRINT_BYTES(ptr, length)				\
+do{ uint8_t line_count = 0;						\
+	for(uint8_t i = 0;i < length; i++) {		\
+		printf("0x%02x, ",ptr[i]); line_count++;\
+		if(line_count == 8) { printf("\n"); }}\
+} while(0); printf("\r\n");
+
+
+
+static const ATCAIfaceCfg cfg_ateccx08a_i2c = {
 	.iface_type			= ATCA_I2C_IFACE,
 	.devtype			= ATECC508A,
 	.slave_address		= ATECC508A_ADDRESS,
@@ -44,7 +54,7 @@ const ATCAIfaceCfg cfg_ateccx08a_i2c = {
 };
 
 //key for hash secret
-const uint8_t transport_key[] = {
+static const uint8_t transport_key[] = {
 	0xf2, 0x11, 0x11, 0x11,
 	0x11, 0x11, 0x11, 0x11,
 	0x11, 0x11, 0x11, 0x11,
@@ -55,26 +65,15 @@ const uint8_t transport_key[] = {
 	0x11, 0x11, 0x11, 0x2f,
 };
 
+int init_atca_device();
 
-class Encryption{
-public:
-	Encryption();
-	~Encryption();
+int setRemote(uint8_t* pubKey);
+int encryptData(uint8_t* input, uint8_t* output, size_t length);
+int decryptData(uint8_t* input, uint8_t* output, size_t length);
 
-	int init();
 
-	int setRemote(uint8_t* pubKey);
-	int encryptData(uint8_t* input, uint8_t* output, size_t length);
-	int decryptData(uint8_t* input, uint8_t* output, size_t length);
-	uint8_t* getPublicKey();
-
-private:
-	uint8_t remote_key[64];
-	uint8_t my_public_key[64];
-	esp_aes_context aes_key;
-
-	int asymmetric_ecdh_comm(uint8_t *remote_key, uint8_t *ecdh_value);
-};
+extern uint8_t* my_public_key;
+esp_aes_context aes_key;
 
 
 #endif
